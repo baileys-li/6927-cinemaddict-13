19049529a14f4b4f07ea8dfd6388979935b81b03
Arthur <arthurlitovko@gmail.com>
2020-11-29 16:42:09 +0200

    Test render block

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-const { container } = require("webpack");
+import {createHeaderProfile} from "./view/header-profile";
 
 const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template)
-}
+  container.insertAdjacentHTML(place, template);
+};
 
+const siteHeaderElement = document.querySelector(`.header`);
 
+render(siteHeaderElement, createHeaderProfile());
