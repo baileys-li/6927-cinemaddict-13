b2cfceab19145104b36de90f1530f3e1fb443996
Arthur <arthurlitovko@gmail.com>
2020-11-29 17:53:26 +0200

    Basic view elements

---
+++ src/view/films-list.js
@@ -0,0 +1,23 @@
+import {createFilmCard} from "./film-card";
+import {createShowMore} from "./show-more-button";
+
+export const createFilmsList = (list) => {
+  const sectionExtraClass = list.isExtra ? `films-list--extra` : ``;
+  const headlineExtraClass = list.isExtra ? `` : `visually-hidden`;
+  const showMoreBtn =  list.isExtra ? `` : createShowMore();
+
+  let films = ``;
+
+  for (let index = 0; index < list.filmsNumber; index++) {
+    films += createFilmCard();
+  }
+
+  return `<section class="films-list ${sectionExtraClass}">
+  <h2 class="films-list__title ${headlineExtraClass}">${list.headline}</h2>
+
+  <div class="films-list__container">
+    ${films}
+  </div>
+  ${showMoreBtn}
+</section>`;
+};
