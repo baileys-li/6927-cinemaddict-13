e93e0f0f3ddb2662da14493188b366b9ab9d02de
Arthur <arthurlitovko@gmail.com>
2020-12-06 00:01:19 +0200

    Better code reading

--- src/view/film-comment.js
+++ src/view/film-comment.js
@@ -3,41 +3,7 @@ import dayjs from "dayjs";
 
 export const createFilmComment = () => {
   const { author, date, emoji, message } = generateComment();
-
-  const returnFormattedDate = () => {
-    const formattedDate = dayjs(date);
-    const currentDate = dayjs();
-
-    const TIME_UNIT = [`year`, `month`, `day`];
-    let output = false;
-
-    TIME_UNIT.forEach((timeUnit) => {
-      if (!output) {
-        output = checkDiff(timeUnit);
-      }
-    });
-
-    return output;
-
-    function checkDiff(timeUnit) {
-      const difference = currentDate.diff(formattedDate, timeUnit);
-      switch (difference) {
-        case 1:
-          return `1 ${timeUnit} ago`;
-          break;
-
-        case 0:
-          return timeUnit === `day` ? `Today` : false;
-          break;
-
-        default:
-          return timeUnit === `year`
-            ? dayjs(date).format(`YYYY/M/D H:m`)
-            : `${difference} ${timeUnit}s ago`;
-          break;
-      }
-    }
-  };
+  const dateText = formatDate(date);
 
   return `<li class="film-details__comment">
   <span class="film-details__comment-emoji">
@@ -47,9 +13,44 @@ export const createFilmComment = () => {
     <p class="film-details__comment-text">${message}</p>
     <p class="film-details__comment-info">
       <span class="film-details__comment-author">${author}</span>
-      <span class="film-details__comment-day">${returnFormattedDate()}</span>
+      <span class="film-details__comment-day">${dateText}</span>
       <button class="film-details__comment-delete">Delete</button>
     </p>
   </div>
 </li>`;
+};
+
+const formatDate = (date) => {
+  const formattedDate = dayjs(date);
+  const currentDate = dayjs();
+
+  const TIME_UNIT = [`year`, `month`, `day`];
+  let output = false;
+
+  TIME_UNIT.forEach((timeUnit) => {
+    if (!output) {
+      output = checkDiff(timeUnit);
+    }
+  });
+
+  return output;
+
+  function checkDiff(timeUnit) {
+    const difference = currentDate.diff(formattedDate, timeUnit);
+    switch (difference) {
+      case 1:
+        return `1 ${timeUnit} ago`;
+        break;
+
+      case 0:
+        return timeUnit === `day` ? `Today` : false;
+        break;
+
+      default:
+        return timeUnit === `year`
+          ? dayjs(date).format(`YYYY/M/D H:m`)
+          : `${difference} ${timeUnit}s ago`;
+        break;
+    }
+  }
 };
